64bcb353dedf273e12e82589a65457ebce0e66fb
Anna Dubinina <anna.dybinina.1@gmail.com>
2022-08-17 12:01:40 +0300

    Added adapter pattern

---
+++ structural/adapter/exercise.js
@@ -0,0 +1,35 @@
+// You are given a class called Square and a function calculateArea() which calculates the area of a given rectangle.
+
+// In order to use Square in calculate_area, instead of augmenting it with width/height members, please implement the SquareToRectangleAdapter class. This adapter takes a square and adapts it in such a way that it can be used as an argument to calculateArea().
+
+class Square {
+  constructor(side) {
+    this.side = side;
+  }
+}
+
+function area(rectangle) {
+  return rectangle.width * rectangle.height;
+}
+
+class SquareToRectangleAdapter {
+  constructor(square) {
+    this.square = square;
+  }
+
+  get width() {
+    return this.square.side;
+  }
+
+  get height() {
+    return this.square.side;
+  }
+}
+
+describe('adapter', function () {
+  it('should adapt things, duh!', function () {
+    let sq = new Square(11);
+    let adapter = new SquareToRectangleAdapter(sq);
+    expect(area(adapter)).toEqual(121);
+  });
+});
